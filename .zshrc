275b2215778a7d0980df390c616e335b9ef5727d
Jewei Mak <jewei.mak@gmail.com>
2023-08-31 00:40:43 +0800

    Message of the day!

--- .zshrc
+++ .zshrc
@@ -6,3 +6,4 @@
 [[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 [[ -s "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && "/opt/homebrew/etc/profile.d/bash_completion.sh"
+[[ -s "~/motd.sh" ]] && "~/motd.sh"
---
+++ motd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# ( fortune -s | cowsay -f small; echo "\nThe world bores you when you're cool." ) | lolcat --seed=82 --spread=2 --freq=0.1
+
+cat << "EOF" | lolcat
+            __:.__
+           (_:..'"=
+            ::/ o o\      Howdy, jewei!
+           ;'-'   (_)   The world bores you   .
+           '-._  ;-'       you're cool !  _'._|\/:
+           .:;  ;                .         '- '   /_
+          :.. ; ;,                \       _/,    "_<
+         :.|..| ;:                 \__   '._____  _)
+         :.|.'| ||                            _/ /
+         :.|..| :'                           `;--:
+         '.|..|:':       _               _ _ :|_\:
+      .. _:|__| '.\.''..' ) ___________ ( )_):|_|:
+:....::''::/  | : :|''| "/ /_=_=_=_=_=/ :_[__'_\3_)
+ ''''      '-''-'-'.__)-'
+EOF
